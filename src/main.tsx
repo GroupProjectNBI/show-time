e858da3cbdc6857a89a91aca734171f0eb187038
ceccav <ceciliacavallin@hotmail.se>
2026-02-10 13:31:27 +0100

    Set up tailwind in the project
    next step is to clean up the files from remaining bootstrap, but
    tailwind package is installed

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider }
   from 'react-router-dom';
-import '../sass/index.scss';
+import '../css/index.css';
 import routes from './routes';
 import App from './App';
 
