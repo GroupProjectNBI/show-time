d6e04b598b725d017d0804d20aafadc69ce9ea55
Grevendev <edvin.lindborg@hotmail.se>
2026-02-11 15:31:45 +0100

    Added Hitta till oss och öppettider med färg

--- src/partials/Footer.tsx
+++ src/partials/Footer.tsx
@@ -1,10 +1,21 @@
-import 'tailwindcss';
+import { Link } from 'react-router-dom';
+import { FaFacebookF, FaInstagram, FaTwitter } from 'react-icons/fa';
 
 export default function Footer() {
   return (
-    <footer className="bg-blue-700 text-white py-4">
-      <div className="container mx-auto text center">
-        © Copyright
+    <footer className="bg-gray-900 text-yellow-500 py-10">
+      <div className="container mx-autopx-4 grid grid-cols-1 md:grid-cols-2 gap-8">
+
+        {/*Hitta till oss*/}
+        <div>
+          <h3 className="text-lg font-bold mb-2">Hitta till oss</h3>
+          <p>Adress:<br />Propellergatan 1<br />211 15 Malmö</p>
+          <p className="mt-4">
+            <strong>Öppettider:</strong><br />
+            Mån - Fre 17:30 - 21:15<br />
+            Lör - Sön 13.30 - 21:15
+          </p>
+        </div>
       </div>
     </footer>
   );
