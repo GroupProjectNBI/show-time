caeee8ec48affb95d6aff67df34a7c2214661af4
KHS1993 <kiflle.selassie@student.nbi-handelsakademin.se>
2026-02-12 15:14:39 +0100

    redo PR untrack file

--- src/routes.ts
+++ src/routes.ts
@@ -8,13 +8,15 @@ import OurVisionPage from './pages/OurVisionPage.tsx';
 import ProductDetailsPage from './pages/ProductDetailsPage.tsx';
 import ProductsPage from './pages/ProductsPage.tsx';
 import AnimalPage from './pages/Animal.tsx';
+import ScreeningsPage from './pages/ScreeningsPage.tsx';
 export default [
   AboutPage,
   NotFoundPage,
   OurVisionPage,
   ProductDetailsPage,
   ProductsPage,
-  AnimalPage
+  AnimalPage,
+  ScreeningsPage
 ]
   // map the route property of each page component to a Route
   .map(x => (({ element: createElement(x), ...x.route }) as Route))
