c552f389667bc82703694731ff5b607bccdde970
Zhaneta-Lecini <zhaneta.lecini@student.nbi-handelsakademin.se>
2026-02-13 12:04:42 +0100

    Add 2 new Endpoints to the ReatApi file

--- src/routes.ts
+++ src/routes.ts
@@ -8,13 +8,17 @@ import OurVisionPage from './pages/OurVisionPage.tsx';
 import ProductDetailsPage from './pages/ProductDetailsPage.tsx';
 import ProductsPage from './pages/ProductsPage.tsx';
 import AnimalPage from './pages/Animal.tsx';
+
+
 export default [
+  
   AboutPage,
   NotFoundPage,
   OurVisionPage,
   ProductDetailsPage,
   ProductsPage,
-  AnimalPage
+  AnimalPage,
+ 
 ]
   // map the route property of each page component to a Route
   .map(x => (({ element: createElement(x), ...x.route }) as Route))
