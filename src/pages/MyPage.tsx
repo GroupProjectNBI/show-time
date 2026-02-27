df1433b11644897bc167466c79296a35212b6a26
Zhaneta-Lecini <zhaneta.lecini@student.nbi-handelsakademin.se>
2026-02-27 13:30:19 +0100

    Fixed merge conflict

--- src/pages/MyPage.tsx
+++ src/pages/MyPage.tsx
@@ -1,6 +1,6 @@
-import { useEffect, useState } from "react";
+
 import { useAuth } from "../context/AuthContext";
-import fetchJson from "../utils/fetchJson";
+
 
 import ChangePasswordForm from "../parts/ChangePasswordForm";
 import UpcomingBookingCard from "../parts/UpcomingBookingCard";
