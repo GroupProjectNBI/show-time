a49d386de460f3b1d7e428a5cd7e13727ce53d78
jarl <jarllindquist@gmail.com>
2026-03-06 08:55:28 +0100

    Fixar vite config: tar bort showtime base path för produktion

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => {
-  return {
-    plugins: [react()],
-    // I produktion vill vi att alla länkar i index.html ska börja med /showtime/
-    base: mode === "production" ? "/showtime/" : "/",
-    server: {
-      proxy: {
-        "/api": {
-          target: "http://localhost:5001", // Din lokala C#-port
-          changeOrigin: true,
-          secure: false,
-        },
+export default defineConfig({
+  plugins: [react()],
+  base: "/", // Du kan även ta bort denna helt om du vill, / är standard
+  server: {
+    proxy: {
+      "/api": {
+        target: "http://localhost:5001", // Din lokala C#-port
+        changeOrigin: true,
+        secure: false,
       },
     },
-  };
+  },
 });
