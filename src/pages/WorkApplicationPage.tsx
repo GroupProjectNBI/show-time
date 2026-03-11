9e1632a450d0c8557b1be7d95ea9a59118d89d14
Grevendev <edvin.lindborg@hotmail.se>
2026-03-11 11:31:06 +0100

    Created the start of 'lediga jobb'page. WIth some styling and button

---
+++ src/pages/WorkApplicationPage.tsx
@@ -0,0 +1,46 @@
+WorkApplication.route = {
+  path: '/lediga-jobb',
+  index: -1 // ska inte synas i headern
+};
+
+export default function WorkApplication() {
+  return (
+    <div className="workapplication-page">
+
+      {/*HERO SECTION */}
+      <section className="relative h-[45vh] md:h-[80vh] w-full overflow-hidden rounded-3xl mb-2">
+        <img
+          src="/images/Commercials/jobbahososs.png"
+          alt="Arbete hos oss"
+          className="absolute inset-0 w-full h-full object-cover scale-110 rounded-3xl"
+        />
+        <div className="absolute inset-0 bg-gradient-to-b from-black/70 to-black/20 rounded-3xl" />
+
+        <div className="relative z-10 flex flex-col justify-end h-full px-6 max-w-6xl mx-auto pb-10">
+          <h1 className="text-4xl md:text-5xl font-bold text-white/75 drop-shadow-lg animate-fadeIn">
+            Lediga jobb
+          </h1>
+          <p className="text-lg md:text-xl text-white/75 mt-2 animate-fadeIn delay-150">
+            Bli en del av vårt team och skapa bioupplevelser som stannar kvar
+          </p>
+
+          {/*BUTTONS*/}
+          <div className="mt-6 flex flex-col sm:flex-row gap-4 animate-fadeIn delay-300">
+            <button
+              className="
+            px-6 py-3 rounded-xl
+            bg-white/10 border border-white/20
+            text-accent font-semibold
+            backdrop-blur-sm
+            hover:bg-white/20 hover:border-white/30
+            transition
+            "
+            >
+              Ansök
+            </button>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
