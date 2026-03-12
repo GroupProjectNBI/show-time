9d48f3db626f3c8482587026c4a1f3ce37eeabc7
Grevendev <edvin.lindborg@hotmail.se>
2026-03-12 16:37:57 +0100

    imported components to AdminPage

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -1,3 +1,15 @@
+import AdminDashboard from "../parts/admin/AdminDashboard";
+import AdminMovies from "../parts/admin/AdminMovies";
+import AdminBookings from "../parts/admin/AdminBookings";
+import AdminScreenings from "../parts/admin/AdminScreenings";
+import AdminTheaters from "../parts/admin/AdminTheaters";
+import AdminUsers from "../parts/admin/AdminUsers";
+import AdminSettings from "../parts/admin/AdminSettings";
+import AdminStaff from "../parts/admin/AdminStaff";
+import AdminSnackMenu from "../parts/admin/AdminSnackMenu";
+
+
+
 export default function AdminPage() {
   return (
     <div className="min-h-screen bg-[#1a1a1a] p-8 flex flex-col items-center">
@@ -5,11 +17,20 @@ export default function AdminPage() {
         Adminpanel
       </h1>
 
-      <div className="w-full max-w-4xl flex flex-col gap-6">
-        {/* Här lägger du admin-moduler */}
+      <div className="w-full max-w-4xl flex flex-col gap-10">
+        <AdminDashboard />
+        <AdminMovies />
+        <AdminBookings />
+        <AdminScreenings />
+        <AdminTheaters />
+        <AdminUsers />
+        <AdminSettings />
+        <AdminStaff />
+        <AdminSnackMenu />
       </div>
     </div>
   );
 }
+
 
 AdminPage.route = { path: "/admin" };
