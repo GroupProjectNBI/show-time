fa6b9bb2bd19e740b02b730608f0cebd6125dcc2
jarl <jarllindquist@gmail.com>
2026-03-14 11:39:23 +0100

    remove click funtion for title on gå på bio

--- src/pages/ScreeningsPage.tsx
+++ src/pages/ScreeningsPage.tsx
@@ -12,7 +12,6 @@ export default function ScreeningsPage() {
   // --- STATE ---
   const [allScreenings, setAllScreenings] = useState<Screening[]>([]);
   const [loading, setLoading] = useState(true);
-  const { create } = useAuth();
 
   // Datumstate - Börjar på dagens datum
   const [selectedDateISO, setSelectedDateISO] = useState<string>(formatDate(new Date()));
@@ -143,7 +142,7 @@ export default function ScreeningsPage() {
 
         {/* HEADER */}
         <div className="mb-8 border-b border-white/10 pb-6">
-          <h1 className="text-2xl font-semibold text-accent uppercase tracking-widest"><Link to="#" onClick={() => create({})} className="shrink-0">Bioprogram</Link></h1>
+          <h1 className="text-2xl font-semibold text-accent uppercase tracking-widest"><Link to="#" className="shrink-0">Bioprogram</Link></h1>
           <p className="mt-1 text-accent/70 max-w-3xl font-light">
             Hitta rätt film för kvällen. Filtrera på genre, salong eller välj ett specifikt datum.
           </p>
