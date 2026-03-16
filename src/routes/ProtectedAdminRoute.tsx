2cb607ef3fb15ec59a42ec2803c43eaa8481c4d6
Grevendev <edvin.lindborg@hotmail.se>
2026-03-16 09:00:47 +0100

    Changed from JSX to ReactNode

--- src/routes/ProtectedAdminRoute.tsx
+++ src/routes/ProtectedAdminRoute.tsx
@@ -1,7 +1,9 @@
 import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/AuthContext";
+import React from "react";
 
-export default function ProtectedAdminRoute({ children }: { children: JSX.Element; }) {
+
+export default function ProtectedAdminRoute({ children }: { children: React.ReactNode; }) {
   const { user, loading } = useAuth();
 
   // Vänta tills AuthContext är färdigladdat
